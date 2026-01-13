32d17a857e2eca40c1c518d487d59c6972faa8c4
Lera <valnovo@Kirills-Mac-mini-2.local>
2026-01-13 19:22:04 +0300

    Replace PNG logo with SVG

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.svg';
 
 export const Footer = () => {
     const location = useLocation();
